2514b2494b51de143f080681e98b6f75cd2d6d3d
Roland Toth <contact@rolandtoth.hu>
2016-04-13 10:27:45 +0200

    Disallow unchecking required radios

--- AdminDeselectRadios.js
+++ AdminDeselectRadios.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
-    $("input:radio:checked").data("chk",true);
-    $("input:radio").click(function(){
-        $("input[name='"+$(this).attr("name")+"']:radio").not(this).removeData("chk");
-        $(this).data("chk",!$(this).data("chk"));
-        $(this).prop("checked",$(this).data("chk"));
+$(document).ready(function () {
+
+    var $radioButtons = $("input:radio");
+
+    $radioButtons.filter(":checked").data("chk", true);
+
+    $radioButtons.click(function () {
+        if ($(this).parents('.InputfieldStateRequired').length) {
+            return true;
+        }
+        $("input[name='" + $(this).attr("name") + "']:radio").not(this).removeData("chk");
+        $(this).data("chk", !$(this).data("chk"));
+        $(this).prop("checked", $(this).data("chk"));
     });
 });
